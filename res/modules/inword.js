586094087f32125fcba283a4d8cc93f85af0761e
wxy000 <396310583@qq.com>
2023-08-10 21:37:22 +0800

    修正页面内刷新无法正确渲染数据的问题

--- res/modules/inword.js
+++ res/modules/inword.js
@@ -1,14 +1,12 @@
 layui.define(function(exports){
-	layui.use(['layer', 'form'], function(){
-		const layer = layui.layer,
-			$ = layui.$,
+	layui.use(['layer', 'form', 'laytpl'], function(){
+		const $ = layui.$,
 			admin = layui.admin,
 			setter = layui.setter,
-			laydate = layui.laydate,
-			element = layui.element,
-			form = layui.form;
+			laytpl = layui.laytpl;
 
 		function getInWord(){
+			let result = [];
 			admin.req({
 				url: setter.mainAddress + 'inword/getRandomWords?nums=' + setter.inwordNum
 				,method: "GET"
@@ -16,14 +14,17 @@ layui.define(function(exports){
 				,cache: false
 				,dataType: "JSON"
 				,done: function(res){
-					for (var i=0;i<res.data.length;i++){
+					/*for (var i=0;i<res.data.length;i++){
 						$("#inword"+(i+1)).html(res.data[i]);
-					}
+					}*/
+					result = res.data;
 				}
 			});
+			return result;
 		}
 
-		function getImg() {
+		function getInImg() {
+			let result = [];
 			admin.req({
 				url: setter.mainAddress + 'inword/getRandomImgs?nums=' + setter.inwordNum
 				,method: "GET"
@@ -31,15 +32,29 @@ layui.define(function(exports){
 				,cache: false
 				,dataType: "JSON"
 				,done: function(res){
-					for (var i=0;i<res.data.length;i++){
+					/*for (var i=0;i<res.data.length;i++){
 						$("#inwordimg"+(i+1)).attr("src", res.data[i]);
-					}
+					}*/
+					result = res.data;
 				}
 			});
+			return result;
 		}
 
-		getInWord();
-		getImg();
+		let getInWords = getInWord();
+		let getInImgs = getInImg();
+
+		let inwordsimgs = [];
+		for (let i=0; i<setter.inwordNum; i++){
+			let tmp = {"inword":getInWords[i],"inimg":getInImgs[i]}
+			inwordsimgs.push(tmp);
+		}
+
+		let inwordTPL = document.getElementById("inwordTPL").innerHTML;
+		let inword = document.getElementById("inword");
+		laytpl(inwordTPL).render(inwordsimgs,function (html){
+			inword.innerHTML = html;
+		});
 
 		//监听窗口尺寸改变事件，控制宽度相同
 		admin.resize(function(){
